2f725bc07f9da58deba4041023bedd48d85a2423
mss-x <44271544+mss-x@users.noreply.github.com>
2021-04-23 11:16:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,37 +2,23 @@
 #cd openwrt
 #
 # 说明：
-# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
+# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了
 #
 
-# 修改openwrt登陆地址,192.168.1.1是默认ip；10.0.0.2是修改的ip
-sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
-
-#设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-#修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
-#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
-#sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
-
-#取消掉feeds.conf.default文件里面的helloworld的#注释
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
-
-#添加额外软件包（自己想要什么就github里面搜索然后添加）
-#git clone 其他github插件源码地址 package/文件夹名称
-#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
-#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
-#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
-
-# Add HelloWorld
+#添加额外软件包
+#openclash出国软件
+#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#clash出国软件
+#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#微信推送
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#HelloWorld
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-
-# Add 京东签到
+#京东签到
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #git clone https://github.com/jerrykuku/node-request.git package/node-request
-
-# Add 解除网易云音乐播放限制
+#解除网易云音乐播放限制
 git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 #添加第三方argon-主题
